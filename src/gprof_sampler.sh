0ef0bdc6e31b449803d176f9d8d921820b1d52e4
h3nnn4n <uber.renan@gmail.com>
2016-12-02 21:59:36 +0000

    Updated the gprof script

--- src/gprof_sampler.sh
+++ src/gprof_sampler.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-#../set_gen/a.out 5 | ./dlx
-#mv gmon.out gmon.sum
+if [[ -e gmon.sum ]]
+then
+    echo -n ''
+else
+    ./dlx >> /dev/null
+    mv gmon.out gmon.sum
+fi
 
-for i in `seq 8 15`
+for i in `seq 4 10`
 do
+    echo -n "$i "
     for j in `seq 1 10`
     do
-        echo $i $j
-        ../set_gen/a.out $i | ./dlx > /dev/null
+        ./dlx $i 1 >> /dev/null
+        ./dlx $i 2 >> /dev/null
+        ./dlx $i 3 >> /dev/null
         gprof -s dlx gmon.out gmon.sum
     done
 done
+echo ''
